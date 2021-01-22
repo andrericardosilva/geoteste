297bd6afbe1a62273baa453b70dc3110be4342c0
andre <andre@lab17340.com.br>
2021-01-22 16:24:14 -0300

    location

--- script.js
+++ script.js
@@ -133,4 +133,23 @@ geojson.eachLayer(function (layer) {
 
 map.fitBounds(geojson.getBounds());
 
+map.locate({ setView: true, maxZoom: 16 });
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+
 
